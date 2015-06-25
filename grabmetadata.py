42b9024408c8baed03bcbae5cd76528c3c2eb360
Robert Crimi <rpcrimi@gmail.com>
2015-06-25 08:57:55 -0600

    metadata implemented

---
+++ grabmetadata.py
@@ -0,0 +1,14 @@
+# Open shell process
+import subprocess
+# Get current working directory
+import os
+import shlex
+
+currentDir = os.getcwd()
+f = currentDir + '/test.nc'
+p  = subprocess.Popen(['./ncdump.sh', f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p2 = subprocess.Popen(shlex.split('grep :standard_name'), stdin=p.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p.stdout.close()
+out, err = p2.communicate()
+
+print out.replace("\t", "").replace("\n", "").split(" ;")
