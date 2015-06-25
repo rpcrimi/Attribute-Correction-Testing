6cbe930c64f3279991028f9c5ccfd08e4492d222
Robert Crimi <rpcrimi@gmail.com>
2015-06-25 09:33:33 -0600

    fixed folder looping

--- loopfolder.py
+++ loopfolder.py
@@ -1,16 +1,14 @@
-import subprocess
 import fnmatch
 import os
 
 currentDir = os.getcwd()
 f = currentDir + '/netCDF'
 
-matches = []
-for root, dirnames, filenames in os.walk(currentDir):
-  for filename in fnmatch.filter(filenames, '*.nc'):
-    matches.append(os.path.join(root, filename))
+def get_nc_files(dir):
+	matches = []
+	for root, dirnames, filenames in os.walk(currentDir):
+		for filename in fnmatch.filter(filenames, '*.nc'):
+			matches.append(os.path.join(root, filename))
+	return matches
 
-for f in matches:
-	p = subprocess.Popen(['./ncdump.sh', f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = p.communicate()
-	print(out or err)
+print get_nc_files(f)
